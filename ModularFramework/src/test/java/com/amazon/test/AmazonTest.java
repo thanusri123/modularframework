8d62fe99a22a7c07c95da17b631217aad6591c72
Thanu Sri <60391581+thanusri123@users.noreply.github.com>
2020-09-29 07:28:05 +0000

    Updates Amazon Test.java from Git

--- ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
+++ ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
@@ -37,7 +37,7 @@ CommonDriver cmnDriver;
 	
 	@Test
 	public void searchProduct() throws Exception{
-		String product = "iPhone";
+		String product = "Apple Watch";
 		String category = "Electronics";
 		
 		homepage.searchProduct(product, category);
